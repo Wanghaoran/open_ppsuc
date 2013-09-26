a725a4d9d78572f522862f3d675d7af892cb0bcb
Wanghaoran <whrlmc@139.com>
2013-09-27 01:21:30 +0800

    更改项目名称
    Signed-off-by: Wanghaoran <whrlmc@139.com>

--- admin/Conf/config.php
+++ admin/Conf/config.php
@@ -2,7 +2,7 @@
 $common_config = include './Public/config.public.php';
 $app_config = array(
   'USER_AUTH_KEY' => 'admin_uid',
-  'USER_AUTH_GATEWAY' => '/Public/login'//验证网关
+  'USER_AUTH_GATEWAY' => '/Public/login',
 );
 return array_merge($common_config, $app_config);
 ?>
