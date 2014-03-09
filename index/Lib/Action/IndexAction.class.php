37f71219e993d08efe5412139382fd5d9784e435
WangHaoRan <whrlmc@139.com>
2014-03-10 00:28:26 +0800

    增加 联盟高校 得 内容

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -3,7 +3,7 @@ class IndexAction extends CommonAction {
   public function index(){
     //新闻
     $News = M('News');
-    $result_news = $News -> alias('n') -> field('n.id,n.title,nc.name as ncname,nc.color,n.addtime') -> join('open_news_category as nc ON n.cid = nc.id') -> order('n.addtime DESC') -> select();
+    $result_news = $News -> alias('n') -> field('n.id,n.title,nc.name as ncname,nc.color,n.addtime') -> join('open_news_category as nc ON n.cid = nc.id') -> limit(12) -> order('n.addtime DESC') -> select();
     $this -> assign('result_news', $result_news);
     $this -> display();
   }
@@ -18,6 +18,14 @@ class IndexAction extends CommonAction {
     public function teacherinfo(){
         $teacher = M('Teacher');
         $result = $teacher -> field('id,name,pic,small_content,big_content') -> find($this -> _get('id', 'intval'));
+        $this -> assign('result', $result);
+        $this -> display();
+    }
+
+    //联盟高校
+    public function unionschool(){
+        $Unionschool = M('Unionschool');
+        $result = $Unionschool -> select();
         $this -> assign('result', $result);
         $this -> display();
     }
